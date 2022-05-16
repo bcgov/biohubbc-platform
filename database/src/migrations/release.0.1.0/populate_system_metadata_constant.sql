d45b8a89a591a89b037691389566d1b377d04706
cgarrettjones <charlie.garrettjones@bayseum.com>
2022-05-16 15:03:11 -0700

    - additional persistance targets to provide source systems with vesioned transforms

--- database/src/migrations/release.0.1.0/populate_system_metadata_constant.sql
+++
@@ -1,5 +0,0 @@
--- populate_system_metadata_constant.sql
-
--- system metadata tombstone data
-insert into system_metadata_constant (constant_name, character_value, description) values ('ORGANIZATION_NAME_FULL', 'Knowledge Management Branch, Ministry of Enivronment and Climate Change Strategy, Government of British Columbia', 'The organizations full name. This value is used in the production of published ecological metadata language files.');
-insert into system_metadata_constant (constant_name, character_value, description) values ('ORGANIZATION_URL', 'https://www2.gov.bc.ca/gov/content/governments/organizational-structure/ministries-organizations/ministries/environment-climate-change', 'The organizations public facing URL. This value is used in the production of published ecological metadata language files.');
--- database/src/migrations/release.0.1.0/populate_system_role.sql
+++
@@ -1,4 +0,0 @@
--- populate_system_role.sql
-insert into system_role (name, record_effective_date, description) values ('System Administrator', now(), '');
-insert into system_role (name, record_effective_date, description) values ('Creator', now(), '');
-insert into system_role (name, record_effective_date, description) values ('Data Administrator', now(), '');
