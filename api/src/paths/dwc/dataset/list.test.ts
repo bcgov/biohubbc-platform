087a16514816e4ac79fea5d2f6998383a6ed7940
Curtis Upshall <curtisupshall@gmail.com>
2022-05-24 11:46:46 -0700

    BHBC-1770: Update list test

--- api/src/paths/dwc/dataset/list.test.ts
+++ api/src/paths/dwc/dataset/list.test.ts
@@ -3,9 +3,9 @@ import { describe } from 'mocha';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import { SubmissionService } from '../../../services/submission-service';
-import * as db from '../../database/db';
-import { getMockDBConnection, getRequestHandlerMocks } from '../../__mocks__/db';
-import * as users from './list';
+import * as db from '../../../database/db';
+import { getMockDBConnection, getRequestHandlerMocks } from '../../../__mocks__/db';
+import { listDataset } from './list';
 
 chai.use(sinonChai);
 
@@ -24,17 +24,27 @@ describe('submissions', () => {
 
       const mockResponse = [
         {
-          id: 1,
-          user_identifier: 'identifier',
-          record_end_date: '',
-          role_ids: [1, 2],
-          role_names: ['System Admin', 'Project Lead']
+          submission_status: "Submission Data Ingested",
+          submission_id: 1,
+          source: "SIMS",
+          uuid: "2267501d-c6a9-43b5-b951-2324faff6397",
+          event_timestamp: "2022-05-24T18:41:42.211Z",
+          delete_timestamp: null,
+          input_key: "platform/1/moose_aerial_stratifiedrandomblock_composition_recruitment_survey_2.5_withdata.zip",
+          input_file_name: "moose_aerial_stratifiedrandomblock_composition_recruitment_survey_2.5_withdata.zip",
+          eml_source: null,
+          darwin_core_source: 'test',
+          create_date: "2022-05-24T18:41:42.056Z",
+          create_user: 15,
+          update_date: "2022-05-24T18:41:42.056Z",
+          update_user: 15,
+          revision_count: 1
         }
       ];
 
       sinon.stub(SubmissionService.prototype, 'listSubmissionRecords').resolves(mockResponse);
 
-      const requestHandler = users.getUserList();
+      const requestHandler = listDataset();
 
       await requestHandler(mockReq, mockRes, mockNext);
 
