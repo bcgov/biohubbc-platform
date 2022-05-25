565cda2d7fda7de4a7e8dcc115600e0e9ff9fa5b
Curtis Upshall <curtisupshall@gmail.com>
2022-05-25 10:13:38 -0700

    BHBC-1770: Add api response scheme valiation to list endpoint.

--- api/src/paths/dwc/dataset/list.ts
+++ api/src/paths/dwc/dataset/list.ts
@@ -38,7 +38,91 @@ GET.apiDoc = {
           schema: {
             type: 'array',
             items: {
-              //
+              title: 'Darwin Core Data Packages Response Object',
+              type: 'object',
+              properties: {
+                submission_status: {
+                  type: 'string',
+                  nullable: true
+                },
+                source: {
+                  type: 'string',
+                  nullable: true
+                },
+                uuid: {
+                  type: 'string'
+                },
+                event_timestamp: {
+                  oneOf: [
+                    {
+                      type: 'string',
+                      nullable: true
+                    },
+                    {
+                      type: 'object'
+                    }
+                  ]
+                },
+                delete_timestamp: {
+                  type: 'string',
+                  nullable: true
+                },
+                input_key: {
+                  type: 'string',
+                  nullable: true
+                },
+                input_file_name: {
+                  type: 'string',
+                  nullable: true
+                },
+                eml_source: {
+                  type: 'string',
+                  nullable: true
+                },
+                darwin_core_source: {
+                  oneOf: [
+                    {
+                      type: 'string',
+                      nullable: true
+                    },
+                    {
+                      type: 'object'
+                    }
+                  ]
+                },
+                created_date: {
+                  oneOf: [
+                    {
+                      type: 'string',
+                      nullable: true
+                    },
+                    {
+                      type: 'object'
+                    }
+                  ]
+                },
+                created_user: {
+                  type: 'number'
+                },
+                update_date: {
+                  oneOf: [
+                    {
+                      type: 'string',
+                      nullable: true
+                    },
+                    {
+                      type: 'object'
+                    }
+                  ]
+                },
+                update_user: {
+                  type: 'number',
+                  nullable: true
+                },
+                revision_count: {
+                  type: 'number'
+                }
+              }
             }
           }
         }
