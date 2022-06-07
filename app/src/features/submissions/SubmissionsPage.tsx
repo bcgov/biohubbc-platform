b9f935cb08f1504d2f2585076c6874c3716a73b9
Curtis Upshall <curtisupshall@gmail.com>
2022-06-07 15:59:34 -0700

    BHBC-1641: Show submissions table

--- app/src/features/submissions/SubmissionsPage.tsx
+++ app/src/features/submissions/SubmissionsPage.tsx
@@ -1,6 +1,18 @@
-import Box from '@material-ui/core/Box';
-import { useApi } from 'hooks/useApi';
 import React from 'react';
+
+
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container'
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography';
+
+import { useApi } from 'hooks/useApi';
 
 const SubmissionsPage = () => {
 
@@ -14,17 +26,58 @@ const SubmissionsPage = () => {
     setLoading(true)
     biohubApi.submissions.listSubmissions().then((res) => {
       setSubmissions(res)
+      console.log('res:', res)
       setLoading(false)
     })
   })
 
+  const makeSignedUrl = (input_key: string): string => {
+    return input_key;
+  }
+
   return (
-    <Box>
-      <span>Submissions Page</span>
-      {loading && (
-        <div>Loading...</div>
-      )}
-      <span>{JSON.stringify(submissions)}</span>
+    <Box my={4}>
+      <Container maxWidth="xl">
+        <Box mb={5} display="flex" justifyContent="space-between">
+          <Typography variant="h1">Submissions</Typography>
+        </Box>
+        <Box>
+          <Paper>
+            <TableContainer>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Filename</TableCell>
+                    <TableCell>Status</TableCell>
+                  </TableRow>
+                </TableHead>
+                {loading ? (
+                  <TableBody>
+                    <Typography variant='body1'>Loading...</Typography>
+                  </TableBody>
+                ) : (
+                  <TableBody data-testid="submissions-table">
+                    {submissions.map((submission) => (
+                      <TableRow>
+                        <TableCell>
+                          <a href={makeSignedUrl(submission.input_key)} target='_blank'>
+                            {submission.input_file_name}
+                          </a>
+                        </TableCell>
+                        <TableCell>
+                          <Box mb={5} display="flex">
+                            {submission.submission_status || <span>Unknown</span>}
+                          </Box>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                )}
+              </Table>
+            </TableContainer>
+          </Paper>
+        </Box>
+      </Container>
     </Box>
   );
 };
