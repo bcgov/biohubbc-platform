05a04084ff939dfef82ddd835b5feafb25c460f6
Curtis Upshall <curtisupshall@gmail.com>
2022-06-07 16:24:29 -0700

    BHBC-1641: Remove console log

--- api/src/app.ts
+++ api/src/app.ts
@@ -150,7 +150,6 @@ function validateAllResponses(req: Request, res: Response, next: NextFunction) {
       let errorList = [];
 
       if (validationResult?.errors) {
-        console.log("BDY:", body)
         validationMessage = `Invalid response for status code ${res.statusCode}`;
 
         errorList = Array.from(validationResult.errors);
