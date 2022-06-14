40d6f554b2c8ef07efe2145dfd11d6a3a10373fd
Upshall <Curtis.Upshall@quartech.com>
2022-06-14 13:55:36 -0700

    BHBC-1641: Refactor tests

---
+++ api/src/services/es-service.test.ts
@@ -0,0 +1,33 @@
+import chai, { expect } from 'chai';
+import { describe } from 'mocha';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import { ESService } from './es-service';
+
+chai.use(sinonChai);
+
+describe('ESService', () => {
+  it('constructs', () => {
+    const esService = new ESService();
+
+    expect(esService).to.be.instanceof(ESService);
+  });
+
+  it('throws an error when getting the Elastic Search client fails', async () => {
+    sinon.stub(ESService.prototype, 'getEsClient').resolves(undefined);
+    const esClient = await new ESService().getEsClient();
+
+    try {
+      await esClient.search();
+      expect.fail();
+    } catch (actualError) {
+      expect((actualError as Error).message).to.equal("Cannot read properties of undefined (reading 'search')");
+    }
+  });
+
+  describe('Elastic Search', () => {
+    afterEach(() => {
+      sinon.restore();
+    });
+  });
+});
