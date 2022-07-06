1fb3a408090d33548b4dcef44f2a67ec39e01d3e
Curtis Upshall <curtisupshall@gmail.com>
2022-07-05 17:10:11 -0700

    BHBC-1835: Remove unused code; Cleanup SearchPage

--- app/src/features/search/SearchPage.tsx
+++ app/src/features/search/SearchPage.tsx
@@ -1,18 +1,8 @@
 import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
-/*
-import Paper from '@material-ui/core/Paper';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-*/
 import Typography from '@material-ui/core/Typography';
 import { IErrorDialogProps } from 'components/dialog/ErrorDialog';
 import { DialogContext } from 'contexts/dialogContext';
-// import AdvancedSearch, { advancedSearchFiltersInitialValues, IAdvancedSearchFilters } from 'components/search-filter/AdvancedSearch';
 import { Formik, FormikProps } from 'formik';
 import { APIError } from 'hooks/api/useAxios';
 import { useApi } from 'hooks/useApi';
@@ -35,11 +25,10 @@ const SearchPage = () => {
   const location = useLocation();
   const dialogContext = useContext(DialogContext);
 
-  // const [isLoading, setIsLoading] = useState(false); // Use dataloader.isLoading
   const searchDataLoader = useDataLoader((query: string) => {
     return biohubApi.search.keywordSearch(query);
   });
-  //  const { isLoading } = searchDataLoader
+  const { isLoading } = searchDataLoader
 
   console.log('searchDataLoader', searchDataLoader);
 
@@ -55,26 +44,6 @@ const SearchPage = () => {
 
   const [formikValues, setFormikValues] = useState<IAdvancedSearch>(collectFilterParams);
   const formikRef = useRef<FormikProps<IAdvancedSearch>>(null);
-
-  console.log('formikValues:', formikValues);
-
-  //Search Params
-  /*
-  React.useEffect(() => {
-    const getParams = async () => {
-      const params = await collectFilterParams();
-      setFormikValues(params);
-    };
-
-    
-    if (isLoading) {
-      setIsLoading(false);
-      getParams();
-    }
-    
-
-  }, [isLoading, location.search, formikValues, collectFilterParams]);
-  */
 
   /**
    * Determines if the search parameters are empty
@@ -144,38 +113,9 @@ const SearchPage = () => {
     });
   };
 
-  /*
-  const results: any[] =
-    searchDataLoader?.data?.map((item) => ({
-      id: item.id,
-      datasetTitle: item.fields.datasetTitle[0]
-    })) || [];
-
-    */
-  //const data =
   const results = (searchDataLoader.data || []).reduce((acc: any, item) => {
     return [...acc, ...item.source.project];
   }, []);
-
-  /*
-  const results = [
-    {
-      id: 0,
-      projectTitle: 'Lorem ipsum',
-      projectObjectives: 'Consectetur adipiscing elit. Nullam in dolor urna. Vivamus efficitur vestibulum mi, eu accumsan lectus sollicitudin venenatis.'
-    },
-    {
-      id: 1,
-      projectTitle: 'Morbi sed',
-      projectObjectives: 'Sed commodo blandit pellentesque. Proin sollicitudin, arcu sit amet tempus mollis.'
-    },
-    {
-      id: 2,
-      projectTitle: 'Duis porta',
-      projectObjectives: 'Mauris porttitor tortor vel venenatis commodo. In congue, dui vel malesuada accumsan, massa mauris rhoncus nunc, sed rhoncus tortor sem quis magna.'
-    },
-  ];
-  */
 
   searchDataLoader.load(formikRef.current?.values.keywords || formikValues.keywords);
 
@@ -190,7 +130,6 @@ const SearchPage = () => {
             BioHubBC Platform search.
           </Typography>
         </Box>
-
         <Box>
           <Formik<IAdvancedSearch>
             innerRef={formikRef}
@@ -201,10 +140,9 @@ const SearchPage = () => {
             <SearchComponent />
           </Formik>
         </Box>
-
         <Box my={4}>
           <Typography variant="h2">
-            {searchDataLoader.isLoading ? (
+            {isLoading ? (
               <>Loading...</>
             ) : (
               <>
@@ -217,10 +155,9 @@ const SearchPage = () => {
             )}
           </Typography>
         </Box>
-
         <Box>
-          {results.map((result: any) => (
-            <Box mb={3} p={2} key={result.id} borderRadius={4} border={1}>
+          {results.map((result: any, index: number) => (
+            <Box mb={3} p={2} key={`${result.projectId}-${index}`} borderRadius={4} border={1}>
               <Typography variant="h4">{result.projectTitle}</Typography>
               <Typography variant="body1" color="textSecondary">
                 {truncate(result.projectObjectives, { length: 200, separator: ' ' })}
