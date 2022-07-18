f3ed62bd0e3c5365b67ff28be97e3f7ebda2f3c1
Nick Phura <nickphura@gmail.com>
2022-07-18 16:16:05 -0700

    Fix app imports

--- api/.pipeline/scripts/clean.js
+++ api/.pipeline/scripts/clean.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const { clean } = require('../lib/clean.js');
-const config = require('./config.js');
+const config = require('../config.js');
 
 const settings = { ...config, phase: config.options.env };
 
