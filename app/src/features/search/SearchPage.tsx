73cae88cab4d68295842885c9c0bd16ae6272f66
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2022-07-28 16:58:45 -0700

    displaying project objectives

--- app/src/features/search/SearchPage.tsx
+++ app/src/features/search/SearchPage.tsx
@@ -188,14 +188,13 @@ const SearchPage = () => {
                       aria-current="page"
                       variant="h4"
                       href={`datasets/${result.datasetId}/details`}
-                    // onClick={() => history.push(`datasets/${result.datasetId}/details`)}
+                      // onClick={() => history.push(`datasets/${result.datasetId}/details`)}
                     >
                       {result.projectTitle}
                     </Link>
                   </Box>
                   <Typography className={classes.datasetAbstract} variant="body1" color="textSecondary">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                    {truncate(result.projectObjectives, { length: 200, separator: ' ' })}
+                    {truncate(result.projectAbstract[0].para, { length: 200, separator: ' ' })}
                   </Typography>
                 </Box>
               </Card>
