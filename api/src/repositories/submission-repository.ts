d2025911ef8521ec93c1095603f295d9400cc213
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2022-08-02 16:55:27 -0700

    clean up

--- api/src/repositories/submission-repository.ts
+++ api/src/repositories/submission-repository.ts
@@ -406,7 +406,7 @@ export class SubmissionRepository extends BaseRepository {
         record_end_date IS NULL;
     `;
 
-    const response = await this.connection.sql<{ uuid: string }>(sqlStatement);
+    const response = await this.connection.sql<IDatasetId>(sqlStatement);
 
     return response.rows;
   }
