de2b812601d44fa9ebea138f6cfd07bbb88ea8d6
AlfredRosenthal <alfred.rosenthal@quartech.com>
2022-08-12 13:00:32 -0700

    download endpoint setup for basic zip file

--- api/src/paths/dwc/spatial/download.ts
+++ api/src/paths/dwc/spatial/download.ts
@@ -1,10 +1,11 @@
+import AdmZip from "adm-zip";
 import { RequestHandler } from "express";
 import { Operation } from "express-openapi";
-import { Feature } from 'geojson';
+// import { Feature } from 'geojson';
 import { getAPIUserDBConnection, getDBConnection } from "../../../database/db";
-import { GeoJSONFeatureCollection } from "../../../openapi/schemas/geoJson";
+// import { GeoJSONFeatureCollection } from "../../../openapi/schemas/geoJson";
 import { defaultErrorResponses } from "../../../openapi/schemas/http-responses";
-import { SpatialService } from "../../../services/spatial-service";
+// import { SpatialService } from "../../../services/spatial-service";
 import { getLogger } from '../../../utils/logger';
 
 const defaultLog = getLogger('paths/dwc/eml/download');
@@ -18,39 +19,9 @@ GET.apiDoc = {
     parameters: [],
     responses: {
         200: {
-          description: 'Spatial components response object.',
+          description: '',
           content: {
-            'application/json': {
-              schema: {
-                type: 'array',
-                items: {
-                  type: 'object',
-                  required: ['submission_spatial_component_id', 'spatial_data'],
-                  properties: {
-                    submission_spatial_component_id: {
-                      type: 'integer',
-                      minimum: 1
-                    },
-                    spatial_data: {
-                      oneOf: [
-                        {
-                          ...GeoJSONFeatureCollection
-                        },
-                        {
-                          type: 'object',
-                          properties: {},
-                          additionalProperties: false,
-                          minProperties: 0,
-                          maxProperties: 0,
-                          description:
-                            'An empty object, representing a spatial component the requester does not have sufficient privileges to view.'
-                        }
-                      ]
-                    }
-                  }
-                }
-              }
-            }
+            'application/zip': {}
           }
         },
         ...defaultErrorResponses
@@ -59,28 +30,56 @@ GET.apiDoc = {
 
 export function downloadSpatialComponents(): RequestHandler {
     return async (req, res) => {
-        const criteria = {
-            type: (req.query.type as string[]) || [],
-            datasetID: (req.query.datasetID as string[]) || [],
-            boundary: JSON.parse(req.query.boundary as string) as Feature
-        };
+        // const criteria = {
+        //     type: (req.query.type as string[]) || [],
+        //     datasetID: (req.query.datasetID as string[]) || [],
+        //     boundary: JSON.parse(req.query.boundary as string) as Feature
+        // };
 
         // what happens if the endpoint is hit without a dataset ID?
-        
+        // naming convention for the zip file? does it matter?
+        // mentions public user, what is that compared to a private user? logging in?
+        // how can I setup my data so things are public/ private
+
         console.log("--- THE NEW HOTNESS DOWNLOAD SPATIAL COMPONENTS ---")
 
         const connection = req['keycloak_token'] ? getDBConnection(req['keycloak_token']) : getAPIUserDBConnection();
 
         try {
             await connection.open();
-            
-            const spatialService = new SpatialService(connection);
+            // const spatialService = new SpatialService(connection);
+            // const response = await spatialService.findSpatialComponentsByCriteria(criteria);
 
-            const response = await spatialService.findSpatialComponentsByCriteria(criteria);
+            /*
+            app.get('/download', (request, response) => {
+            const fileData = 'SGVsbG8sIFdvcmxkIQ=='
+            const fileName = 'hello_world.txt'
+            const fileType = 'text/plain'
+
+            response.writeHead(200, {
+                'Content-Disposition': `attachment; filename="${fileName}"`,
+                'Content-Type': fileType,
+            })
+
+            const download = Buffer.from(fileData, 'base64')
+            response.end(download)
+            })
+            */
 
             await connection.commit();
 
-            res.status(200).json(response.map((item) => item.spatial_component));
+            const zip = new AdmZip();
+            zip.addFile(Date.now() + '.json', Buffer.from(JSON.stringify({msg: "Look at this bad boii"})));
+            const zipToSend = zip.toBuffer();
+
+            // res.status(200).json(response.map((item) => item.spatial_component));
+            // res.status(200).sendFile(zipToSend.toString())
+            res.writeHead(200, {
+                'Content-Disposition': `attachment; filename="download.zip"`,
+                'Content-Type': 'application/zip'
+            })
+            .end(zipToSend)
+            // res.status(200).end(zipToSend)
         } catch (error) {
             defaultLog.error({ label: 'downloadSpatialComponents', message: 'error', error})
             await connection.rollback();
