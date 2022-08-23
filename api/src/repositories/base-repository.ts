a6ce17aaf79fb72bfff7e8ffe0c3992de8e53763
AlfredRosenthal <alfred.rosenthal@quartech.com>
2022-08-23 09:31:21 -0700

    added admin function to base repo

--- api/src/repositories/base-repository.ts
+++ api/src/repositories/base-repository.ts
@@ -1,4 +1,6 @@
+import { SYSTEM_ROLE } from '../constants/roles';
 import { IDBConnection } from '../database/db';
+import { UserService } from '../services/user-service';
 
 /**
  * Base class for repositories.
@@ -11,5 +13,11 @@ export class BaseRepository {
 
   constructor(connection: IDBConnection) {
     this.connection = connection;
+  }
+
+  async isAdmin(id: number): Promise<boolean> {
+    const userService = new UserService(this.connection)
+    const userObject = await userService.getUserById(id);
+    return [SYSTEM_ROLE.SYSTEM_ADMIN, SYSTEM_ROLE.DATA_ADMINISTRATOR].some((systemRole) => userObject.role_names.includes(systemRole))
   }
 }
