9c6a9117b29b2b992141ad6cec1865e67d73ffc4
Curtis Upshall <curtisupshall@gmail.com>
2022-10-13 12:42:32 -0700

    BHBC-1953: Add type defs for taxon API hooks

---
+++ app/src/interfaces/useTaxonomyApi.interface.ts
@@ -0,0 +1,14 @@
+export interface ITaxonomySearchResponse {
+    searchResponse: Array<{
+        id: string;
+        code: string;
+        label: string;
+    }>
+};
+
+export interface ITaxonomyListRespone {
+    searchResponse: Array<{
+        id: string;
+        label: string;
+    }>
+};
