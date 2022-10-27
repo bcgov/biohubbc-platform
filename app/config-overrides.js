a1b1fb30200367a2534cfd47c6e3b7090a65d2bc
Nick Phura <nickphura@gmail.com>
2022-10-27 16:50:02 -0700

    Fix jest test issues

--- app/config-overrides.js
+++ app/config-overrides.js
@@ -1,19 +1,24 @@
-module.exports = function override(config, env) {
-  config.resolve.fallback = {
-    assert: require.resolve('assert'),
-    // buffer: require.resolve('buffer'),
-    constants: require.resolve('fs-constants'),
-    fs: require.resolve('fs-extra'),
-    path: require.resolve('path-browserify'),
-    stream: require.resolve('stream-browserify')
-  };
+module.exports = {
+  webpack: function (config, env) {
+    config.resolve.fallback = {
+      assert: require.resolve('assert'),
+      // buffer: require.resolve('buffer'),
+      constants: require.resolve('fs-constants'),
+      fs: require.resolve('fs-extra'),
+      path: require.resolve('path-browserify'),
+      stream: require.resolve('stream-browserify')
+    };
 
-  //   config.plugins.push(
-  //     new webpack.ProvidePlugin({
-  //       process: 'process/browser',
-  //       Buffer: ['buffer', 'Buffer']
-  //     })
-  //   );
+    // Hide sourcemap (development) warnings in app console log
+    config.ignoreWarnings = [/Failed to parse source map/, /autoprefixer/];
 
-  return config;
+    //   config.plugins.push(
+    //     new webpack.ProvidePlugin({
+    //       process: 'process/browser',
+    //       Buffer: ['buffer', 'Buffer']
+    //     })
+    //   );
+
+    return config;
+  }
 };
