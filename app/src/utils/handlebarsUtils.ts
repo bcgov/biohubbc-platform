5455ff05b78a4e587a648cb9ff94d50af5dbff61
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2022-10-31 16:43:51 -0700

    function added to convert precompiled template

--- app/src/utils/handlebarsUtils.ts
+++ app/src/utils/handlebarsUtils.ts
@@ -1,6 +1,10 @@
 import Handlebars, { HelperOptions } from 'handlebars';
 
 export const useHandlebars = () => {
+  /**
+   *This functions allows to conditionally check values
+   *
+   */
   const applyConditionalChecks = () => {
     Handlebars.registerHelper('ifCond', (v1, operator, v2, options: HelperOptions) => {
       switch (operator) {
@@ -30,23 +34,34 @@ export const useHandlebars = () => {
     });
   };
 
-  const compileFromRawTemplate = (template: any): HandlebarsTemplateDelegate => {
+  const compileFromRawTemplate = (template: string): HandlebarsTemplateDelegate => {
     applyConditionalChecks();
     return Handlebars.compile(template);
   };
 
-  // const parsedHbr = Handlebars.parse(hbr);
+  /**
+   *This call requires a precompiled string from the DB, derived with the following steps,
+   * functionally similar to:
+   *
+   * registeredConditions: useHandlebars().applyConditionalChecks();
+   * parse: Handlebars.parse(rawTemplate);
+   * preCompile" : Handlebars.precompile(parsedHbr);
+   *
+   * @param {TemplateSpecification} preCompiledtemplate
+   * @return {*}  {HandlebarsTemplateDelegate}
+   */
+  const compileFromPrecompiledTemplate = (preCompiledtemplate: TemplateSpecification): HandlebarsTemplateDelegate => {
+    const encodedHandlebarsFunction = `(handlebars) => handlebars.template(${preCompiledtemplate})`;
+    const handlebarsFunction = eval(encodedHandlebarsFunction);
+    const template = handlebarsFunction(Handlebars);
 
-  // const preCompiled = Handlebars.precompile(parsedHbr);
-  // const encodedHandlebarsFunction = `(handlebars) => handlebars.template(${preCompiled})`;
-
-  // const handlebarsFunction = eval(encodedHandlebarsFunction);
-  // const template = handlebarsFunction(Handlebars);
-
-  // result = template(data);
+    return template;
+  };
 
   return {
-    compileFromRawTemplate
+    compileFromRawTemplate,
+    compileFromPrecompiledTemplate,
+    applyConditionalChecks
   };
 };
 
