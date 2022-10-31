a3ba7e8bd41c208b92a2648916ea3d3ad23f016e
Nick Phura <nickphura@gmail.com>
2022-10-31 11:50:42 -0700

    Move react-app-rewired + related dependencies to dev dependencies. Run `npm audit fix`

--- app/config-overrides.js
+++ app/config-overrides.js
@@ -2,7 +2,6 @@ module.exports = {
   webpack: function (config, env) {
     config.resolve.fallback = {
       assert: require.resolve('assert'),
-      // buffer: require.resolve('buffer'),
       constants: require.resolve('fs-constants'),
       fs: require.resolve('fs-extra'),
       path: require.resolve('path-browserify'),
@@ -11,13 +10,6 @@ module.exports = {
 
     // Hide sourcemap (development) warnings in app console log
     config.ignoreWarnings = [/Failed to parse source map/, /autoprefixer/];
-
-    //   config.plugins.push(
-    //     new webpack.ProvidePlugin({
-    //       process: 'process/browser',
-    //       Buffer: ['buffer', 'Buffer']
-    //     })
-    //   );
 
     return config;
   }
