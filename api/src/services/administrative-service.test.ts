5741ca81737317b99f8ab7d4b1aed0a5d8942e70
Kjartan <kjartanreinarsson@gmail.com>
2022-11-09 17:14:04 -0800

    fix service tests

--- api/src/services/administrative-service.test.ts
+++ api/src/services/administrative-service.test.ts
@@ -1,34 +1,95 @@
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
 import { describe } from 'mocha';
-import { default as Sinon, default as sinon } from 'sinon';
-import { HTTPError } from '../errors/http-error';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import {
+  AdministrativeRepository,
+  IAdministrativeActivity,
+  ICreateAdministrativeActivity
+} from '../repositories/administrative-repository';
 import { getMockDBConnection } from '../__mocks__/db';
 import { AdministrativeService } from './administrative-service';
+
+chai.use(sinonChai);
 
 describe('administrativeService', () => {
   describe('updateAdministrativeActivity', () => {
     afterEach(() => {
-      Sinon.restore();
+      sinon.restore();
     });
 
     const dbConnectionObj = getMockDBConnection();
     const administrativeService = new AdministrativeService(dbConnectionObj);
 
-    it('should throw a 500 error when failed to update administrative activity', async () => {
-      const mockQuery = sinon.stub();
+    it('should return id value', async () => {
+      const AdministrativeRepositoryStub = sinon
+        .stub(AdministrativeRepository.prototype, 'updateAdministrativeActivity')
+        .resolves({ id: 1 });
 
-      mockQuery.resolves({
-        rowCount: null
-      });
+      const result = await administrativeService.updateAdministrativeActivity(1, 2);
 
-      try {
-        await administrativeService.updateAdministrativeActivity(1, 2);
+      expect(result).to.eql({ id: 1 });
+      expect(AdministrativeRepositoryStub).to.have.been.calledOnce;
+    });
+  });
 
-        expect.fail();
-      } catch (actualError) {
-        expect((actualError as HTTPError).status).to.equal(500);
-        expect((actualError as HTTPError).message).to.equal('Failed to update administrative activity');
-      }
+  describe('getPendingAccessRequestCount', () => {
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    const dbConnectionObj = getMockDBConnection();
+    const administrativeService = new AdministrativeService(dbConnectionObj);
+
+    it('should return id value', async () => {
+      const AdministrativeRepositoryStub = sinon
+        .stub(AdministrativeRepository.prototype, 'getPendingAccessRequestCount')
+        .resolves(1);
+
+      const result = await administrativeService.getPendingAccessRequestCount('string');
+
+      expect(result).to.eql(1);
+      expect(AdministrativeRepositoryStub).to.have.been.calledOnce;
+    });
+  });
+
+  describe('createAdministrativeActivity', () => {
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    const dbConnectionObj = getMockDBConnection();
+    const administrativeService = new AdministrativeService(dbConnectionObj);
+
+    it('should return id value', async () => {
+      const AdministrativeRepositoryStub = sinon
+        .stub(AdministrativeRepository.prototype, 'createAdministrativeActivity')
+        .resolves({ id: 1 } as unknown as ICreateAdministrativeActivity);
+
+      const result = await administrativeService.createAdministrativeActivity(1, 'string');
+
+      expect(result).to.eql({ id: 1 });
+      expect(AdministrativeRepositoryStub).to.have.been.calledOnce;
+    });
+  });
+
+  describe('getAdministrativeActivities', () => {
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    const dbConnectionObj = getMockDBConnection();
+    const administrativeService = new AdministrativeService(dbConnectionObj);
+
+    it('should return id value', async () => {
+      const AdministrativeRepositoryStub = sinon
+        .stub(AdministrativeRepository.prototype, 'getAdministrativeActivities')
+        .resolves([{ id: 1 } as unknown as IAdministrativeActivity]);
+
+      const result = await administrativeService.getAdministrativeActivities('string', ['string']);
+
+      expect(result).to.eql([{ id: 1 }]);
+      expect(AdministrativeRepositoryStub).to.have.been.calledOnce;
     });
   });
 });
