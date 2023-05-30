b83ded03382e9e8cb148961624475a1a477c6600
Alfred Rosenthal <alfred.k.rosenthal@gmail.com>
2023-05-30 11:14:34 -0700

    ran fix

--- api/src/paths/administrative/review/list.ts
+++ api/src/paths/administrative/review/list.ts
@@ -14,7 +14,7 @@ export const GET: Operation = [
     return {
       and: [
         {
-          validSystemRoles: [SYSTEM_ROLE.SYSTEM_ADMIN , SYSTEM_ROLE.DATA_ADMINISTRATOR],
+          validSystemRoles: [SYSTEM_ROLE.SYSTEM_ADMIN, SYSTEM_ROLE.DATA_ADMINISTRATOR],
           discriminator: 'SystemRole'
         }
       ]
