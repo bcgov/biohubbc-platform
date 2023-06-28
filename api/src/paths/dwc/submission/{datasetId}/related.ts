ef3536ed572021206ab74932b29d9af4bb96ed25
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2023-06-28 10:12:00 -0700

    remove logs

--- api/src/paths/dwc/submission/{datasetId}/related.ts
+++ api/src/paths/dwc/submission/{datasetId}/related.ts
@@ -105,13 +105,10 @@ export function getRelatedDatasetsByDatasetId(): RequestHandler {
         })
       );
 
-      console.log('datasetsWithSupplementaryData', datasetsWithSupplementaryData);
-
       await connection.commit();
 
       res.status(200).json({ datasetsWithSupplementaryData });
     } catch (error) {
-      console.log('error is: ', error);
       defaultLog.error({ label: 'getRelatedDatasetsByDatasetId', message: 'error', error });
       await connection.rollback();
       throw error;
