6995888a3dabb3c55d26a581aea0f8d44f1b8979
Curtis Upshall <curtisupshall@gmail.com>
2023-12-22 09:41:17 -0800

    Removed Add Security menu

--- app/src/components/security/ManageSecurity.tsx
+++ app/src/components/security/ManageSecurity.tsx
@@ -1,10 +1,8 @@
-import { mdiChevronDown, mdiChevronUp, mdiLock, mdiLockOpenOutline, mdiSecurity } from '@mdi/js';
+import { mdiSecurity } from '@mdi/js';
 import Icon from '@mdi/react';
-import { Button, Menu, MenuItem } from '@mui/material';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
+import { Button } from '@mui/material';
 import { useSubmissionContext } from 'hooks/useContext';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import SecuritiesDialog from './SecuritiesDialog';
 import UnsecureDialog from './UnsecureDialog';
 
@@ -18,18 +16,8 @@ const ManageSecurity = (props: IManageSecurityProps) => {
 
   const hasSecurity = Boolean(submissionFeaturesAppliedRulesDataLoader.data?.length);
 
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const [isUnsecureDialogOpen, setIsUnsecuredDialogOpen] = useState(false);
   const [isSecuritiesDialogOpen, setIsSecuritiesDialogOpen] = useState(false);
-
-  const open = Boolean(anchorEl);
-
-  const handleMenuClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-  };
 
   return (
     <>
@@ -39,7 +27,6 @@ const ManageSecurity = (props: IManageSecurityProps) => {
         onClose={() => {
           props.onClose();
           setIsSecuritiesDialogOpen(false);
-          handleMenuClose();
         }}
       />
       <UnsecureDialog
@@ -48,43 +35,16 @@ const ManageSecurity = (props: IManageSecurityProps) => {
         onClose={() => {
           props.onClose();
           setIsUnsecuredDialogOpen(false);
-          handleMenuClose();
         }}
       />
       <Button
         color="primary"
         data-testid="manage-security"
         variant="outlined"
-        onClick={handleMenuClick}
-        startIcon={<Icon path={mdiSecurity} size={0.75} />}
-        endIcon={open ? <Icon path={mdiChevronUp} size={0.75} /> : <Icon path={mdiChevronDown} size={0.75} />}>
+        onClick={() => setIsSecuritiesDialogOpen(true)}
+        startIcon={<Icon path={mdiSecurity} size={0.75} />}>
         {hasSecurity ? 'Edit Security' : 'Add Security'}
       </Button>
-      <Menu
-        open={open}
-        anchorEl={anchorEl}
-        onClose={handleMenuClose}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'right'
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'right'
-        }}>
-        <MenuItem onClick={() => setIsSecuritiesDialogOpen(true)}>
-          <ListItemIcon>
-            <Icon path={mdiLock} size={0.75} />
-          </ListItemIcon>
-          <ListItemText>Secure Records</ListItemText>
-        </MenuItem>
-        <MenuItem onClick={() => setIsUnsecuredDialogOpen(true)}>
-          <ListItemIcon>
-            <Icon path={mdiLockOpenOutline} size={0.75} />
-          </ListItemIcon>
-          <ListItemText>Unsecure Records</ListItemText>
-        </MenuItem>
-      </Menu>
     </>
   );
 };
