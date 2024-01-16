aabed16ba10043a3a5de6c94f355da08b38df331
Nick Phura <nickphura@gmail.com>
2024-01-15 21:45:49 -0800

    Add test. Fix multer clamav scan

--- api/src/app.ts
+++ api/src/app.ts
@@ -54,30 +54,36 @@ const openAPIFramework = initialize({
   docsPath: '/raw-api-docs', // path to view raw openapi spec
   consumesMiddleware: {
     'application/json': express.json({ limit: MAX_REQ_BODY_SIZE }),
-    'multipart/form-data': function (req, res, next) {
+    'multipart/form-data': async function (req, res, next) {
+      console.log('AA');
       const multerRequestHandler = multer({
         storage: multer.memoryStorage(), // TOOD change to local/PVC storage and stream file uploads to S3?
         limits: { fileSize: MAX_UPLOAD_FILE_SIZE }
       }).array('media', MAX_UPLOAD_NUM_FILES);
-
-      multerRequestHandler(req, res, (error?: any) => {
+      console.log('BB');
+      return multerRequestHandler(req, res, async function (error?: any) {
+        console.log('CC');
         if (error) {
           return next(error);
         }
-
+        console.log('DD');
         if (req.files && req.files.length) {
+          console.log('EE');
           // Set original request file field to empty string to satisfy OpenAPI validation
           // See: https://www.npmjs.com/package/express-openapi#argsconsumesmiddleware
-          (req.files as Express.Multer.File[]).forEach((file) => {
+          for (const file of req.files as Express.Multer.File[]) {
             req.body[file.fieldname] = '';
-
+            console.log('FF');
+            console.log(file);
             // Scan file for malicious content, if enabled
-            if (!scanFileForVirus(file)) {
+            const isFileSafe = await scanFileForVirus(file);
+            console.log('!!!!!');
+            if (!isFileSafe) {
               throw new HTTP400('Malicious file content detected.', [{ file_name: file.originalname }]);
             }
-          });
+          }
         }
-
+        console.log('GG');
         return next();
       });
     },
--- api/src/repositories/code-repository.test.ts
+++ api/src/repositories/code-repository.test.ts
@@ -73,6 +73,61 @@ describe('CodeRepository', () => {
   });
 
   describe('getFeaturePropertyByName', () => {
-    // @TODO
-  })
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    it('throws an error if no matching record found', async () => {
+      const mockQueryResponse = {
+        rowCount: 0,
+        rows: []
+      } as any as Promise<QueryResult<any>>;
+
+      const mockDBConnection = getMockDBConnection({
+        sql: () => mockQueryResponse
+      });
+
+      const codeRepository = new CodeRepository(mockDBConnection);
+
+      const featurePropertyName = 'name';
+
+      try {
+        await codeRepository.getFeaturePropertyByName(featurePropertyName);
+
+        expect.fail();
+      } catch (error) {
+        expect((error as Error).message).to.equal('Failed to get feature property record');
+      }
+    });
+
+    it('should return row if succeeds', async () => {
+      const mockRow: FeatureTypeCode & FeaturePropertyCode = {
+        feature_type_id: 1,
+        feature_type_name: 'dataset',
+        feature_type_display_name: 'Dataset',
+        feature_property_id: 2,
+        feature_property_name: 'name',
+        feature_property_display_name: 'Name',
+        feature_property_type_id: 3,
+        feature_property_type_name: 'string'
+      };
+
+      const mockQueryResponse = {
+        rowCount: 1,
+        rows: [mockRow]
+      } as any as Promise<QueryResult<any>>;
+
+      const mockDBConnection = getMockDBConnection({
+        sql: () => mockQueryResponse
+      });
+
+      const codeRepository = new CodeRepository(mockDBConnection);
+
+      const featurePropertyName = 'name';
+
+      const result = await codeRepository.getFeaturePropertyByName(featurePropertyName);
+
+      expect(result).to.be.eql(mockRow);
+    });
+  });
 });
