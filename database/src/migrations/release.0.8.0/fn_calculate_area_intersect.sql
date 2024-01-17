6228e283a17f1020a532ce3af936b021acef8f3e
Alfred Rosenthal <alfred.k.rosenthal@gmail.com>
2024-01-17 12:40:43 -0800

    added examples and better documentation

--- database/src/migrations/release.0.8.0/fn_calculate_area_intersect.sql
+++ database/src/migrations/release.0.8.0/fn_calculate_area_intersect.sql
@@ -1,7 +1,17 @@
 -- *******************************************************************
 -- Procedure: fn_calculate_area_intersect
--- Purpose: returns true if both geometries intersect by the given tolerance amount (0.4 == 40% coverage)
--- Assumptions: This function assumes that geom1 will always be a Polygon/ MultiPolygon provided by the system `region_lookup` table.
+-- Purpose: This function accepts 2 geometries and calculates the area of intersection between them. 
+-- 					Once calculated it then compares the percentage of area intersecting to the tolerance value.
+-- 					Meaning that intersections of a certain amount will be considering 'intersecting'
+-- Example: select fn_calculate_area_intersect('POLYGON((0 0, 0 10, 10 10, 10 0, 0 0))'::geometry, 'POLYGON((5 5, 5 15, 15 15, 15 5, 5 5))'::geometry, 0.2);
+-- 					In this example, I expect the function to return true because at least 20% (tolerance of 0.2) of these two geometries are intersecting. 
+-- 					
+-- 					select fn_calculate_area_intersect('POLYGON((0 0, 0 10, 10 10, 10 0, 0 0))'::geometry, 'POLYGON((5 5, 5 15, 15 15, 15 5, 5 5))'::geometry, 0.3);
+--					In this example, I expect the function to return false because the area of intersection is onl 25% not the required 30% (tolerance of 0.3)
+-- 					
+--					The data in these examples can be visualized in DBeaver with the below sql statement. All these examples can be run in a SQL editor for verification.
+-- 					select st_collect('POLYGON((0 0, 0 10, 10 10, 10 0, 0 0))'::geometry, 'POLYGON((5 5, 5 15, 15 15, 15 5, 5 5))'::geometry);
+-- Assumptions: This function assumes that geom1 will always be a Polygon/ MultiPolygon provided by the systems `region_lookup` table.
 --
 -- MODIFICATION HISTORY
 -- Person           Date        Comments
