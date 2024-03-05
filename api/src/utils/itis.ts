b5fd23ed73df4b5e3bd64c3f2a34c41887f57cad
Macgregor Aubertin-Young <macgregor.aubertin-young@gov.bc.ca>
2024-03-05 08:14:00 -0800

    comments on itis sorting functions

--- api/src/utils/itis.ts
+++ api/src/utils/itis.ts
@@ -1,32 +1,36 @@
 import { TaxonSearchResult } from '../services/taxonomy-service';
 
 /**
- * Sorts the ITIS response such that exact matches are at the start of the array of matching taxa
+ * Sorts the ITIS response by how strongly records match the search terms
  *
  * @param {ItisSolrSearchResponse[]} data
+ * @param {string[]} searchTerms
  * @memberof ItisService
  */
 export const sortExactMatches = (data: TaxonSearchResult[], searchTerms: string[]): TaxonSearchResult[] => {
   const searchTermsLower = searchTerms.map((item) => item.toLowerCase());
 
-  // Prioritize taxa where any word in the scientific or common name matches any of the search terms
+  // Prioritize taxa where any word in the scientific or common name matches ANY of the search terms
   // eg. ['Black', 'bear'] -> "Black" matches on "Black widow"
-  const contains = customSortContainsAnyMatchingSearchTerm(data, searchTermsLower);
-
-  // Prioritize records where any word in the scientific or common name matches the JOINED search terms
-  const someEquals = customSortContainsSearchTermsJoined(contains, searchTermsLower);
+  const containsAnyMatch = customSortContainsAnyMatchingSearchTerm(data, searchTermsLower);
 
   // Prioritize taxa where either the scientific name or any common name CONTAINS the search terms joined
   // eg. ['Black', 'bear'] -> "Black bear" matches on "American black bear"
-  const exactEquals = customSortContainsSearchTermsJoined(someEquals, searchTermsLower);
+  const containsAnyMatchJoined = customSortContainsSearchTermsJoinedExact(containsAnyMatch, searchTermsLower);
 
-  return exactEquals;
+  // Prioritize taxa where either the scientific name or any common name is EXACTLY EQUAL to the search terms joined
+  // eg. ['Wolf'] -> "Wolf" is prioritized over "Forest Wolf"
+  const exactlyEquals = customSortEqualsSearchTermsExact(containsAnyMatchJoined, searchTermsLower);
+
+  return exactlyEquals;
 };
 
 /**
- * Sorts the ITIS response such that exact matches with search terms are first
+ * Sorts the ITIS response to prioritize records where any word in the scientific or
+ * common name matches ANY of the search terms
  *
  * @param {ItisSolrSearchResponse[]} data
+ * @param {string[]} searchTerms
  * @memberof ItisService
  */
 export const customSortContainsAnyMatchingSearchTerm = (
@@ -48,8 +52,7 @@ export const customSortContainsAnyMatchingSearchTerm = (
   };
 
   const checkForMatch = (item: TaxonSearchResult, searchTerms: string[]) => {
-    const commonNameWords = item.commonNames.flatMap((name: string) => name.toLowerCase().split(/\s+/)) ?? [];
-
+    const commonNameWords = item.commonNames?.flatMap((name: string) => name.toLowerCase().split(/\s+/));
     const scientificNameWords = item.scientificName.toLowerCase().split(/\s+/);
 
     // Check if any word in commonNames or scientificName matches any word in searchTerms
@@ -62,49 +65,14 @@ export const customSortContainsAnyMatchingSearchTerm = (
 };
 
 /**
- * Sorts the ITIS response such that exact matches with search terms are first
+ * Sorts the ITIS response to prioritize records where either the scientific name or
+ * any common name CONTAINS the search terms joined
  *
  * @param {ItisSolrSearchResponse[]} data
+ * @param {string[]} searchTerms
  * @memberof ItisService
  */
-export const customSortContainsSearchTermsJoined = (
-  data: TaxonSearchResult[],
-  searchTerms: string[]
-): TaxonSearchResult[] => {
-  // Custom sorting function
-  const customSort = (a: TaxonSearchResult, b: TaxonSearchResult) => {
-    const aInReference = checkForMatch(a, searchTerms);
-    const bInReference = checkForMatch(b, searchTerms);
-
-    if (aInReference && !bInReference) {
-      return -1; // Place items from searchTerms before other items
-    } else if (!aInReference && bInReference) {
-      return 1; // Place other items after items from searchTerms
-    } else {
-      return 0; // Maintain the original order if both are from searchTerms or both are not
-    }
-  };
-
-  // Function to check if an item is a match with search terms
-  const checkForMatch = (item: TaxonSearchResult, searchTerms: string[]) => {
-    const commonNameWords = item.commonNames?.map((name) => name.toLowerCase());
-
-    // Lowercase scientificName and split into individual words
-    const scientificNameWords = item.scientificName.toLowerCase().split(/\s+/);
-
-    return commonNameWords?.includes(searchTerms.join(' ')) || scientificNameWords.includes(searchTerms.join(' '));
-  };
-
-  return data.sort(customSort);
-};
-
-/**
- * Sorts the ITIS response such that exact matches with search terms are first
- *
- * @param {ItisSolrSearchResponse[]} data
- * @memberof ItisService
- */
-export const customSortContainsSearchTermsJoined = (
+export const customSortContainsSearchTermsJoinedExact = (
   data: TaxonSearchResult[],
   searchTerms: string[]
 ): TaxonSearchResult[] => {
@@ -123,8 +91,7 @@ export const customSortContainsSearchTermsJoined = (
 
   // Function to check if an item is a match with search terms
   const checkForMatch = (item: TaxonSearchResult, searchTerms: string[]) => {
-    const commonNameWords = item.commonNames.map((name: string) => name.toLowerCase());
-
+    const commonNameWords = item.commonNames?.map((name: string) => name.toLowerCase());
     const scientificNameWord = item.scientificName.toLowerCase();
 
     // Add a space such that "Black bear" matches "American black bear" and not "Black Bearded"
@@ -132,6 +99,41 @@ export const customSortContainsSearchTermsJoined = (
       scientificNameWord === searchTerms.join(' ') ||
       commonNameWords?.some((name) => `${name}${' '}`.includes(`${searchTerms.join(' ')}${' '}`))
     );
+  };
+
+  return data.sort(customSort);
+};
+
+/**
+ * Sorts the ITIS response to prioritize taxa where either the scientific name or
+ * any common name is EXACTLY EQUAL to the search terms joined
+ *
+ * @param {ItisSolrSearchResponse[]} data
+ * @memberof ItisService
+ */
+export const customSortEqualsSearchTermsExact = (
+  data: TaxonSearchResult[],
+  searchTerms: string[]
+): TaxonSearchResult[] => {
+  const customSort = (a: TaxonSearchResult, b: TaxonSearchResult) => {
+    const aInReference = checkForMatch(a, searchTerms);
+    const bInReference = checkForMatch(b, searchTerms);
+
+    if (aInReference && !bInReference) {
+      return -1; // Place items from searchTerms before other items
+    } else if (!aInReference && bInReference) {
+      return 0; // Maintain the original order
+    } else {
+      return 0; // Maintain the original order
+    }
+  };
+
+  // Function to check if an item is a match with search terms
+  const checkForMatch = (item: TaxonSearchResult, searchTerms: string[]) => {
+    const commonNameWords = item.commonNames?.map((name: string) => name.toLowerCase());
+    const scientificNameWord = item.scientificName.toLowerCase();
+
+    return scientificNameWord === searchTerms.join(' ') || commonNameWords?.includes(searchTerms.join(' '));
   };
 
   return data.sort(customSort);
