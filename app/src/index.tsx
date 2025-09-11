434d04652af4a091e612dbc299a2ee924b09eb7e
Macgregor Aubertin-Young <macgregor.aubertin-young@gov.bc.ca>
2025-09-11 12:48:10 -0700

    initial router restructure

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,10 +1,10 @@
 import App from 'App';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container!);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+root.render(<App />);
+
 serviceWorker.unregister();
