db17495e7692197ab7c3190e05d202044dc3e48b
Macgregor Aubertin-Young <108430771+mauberti-bc@users.noreply.github.com>
2026-01-14 14:11:47 -0800

    SIMSBIOHUB-782: Let Contributor Systems Publish Codesets (#270)
    * codeset serivce and repository
    
    * sql fixes for inserting codesets
    
    * post new codeset
    
    * add tests
    
    * fix bad foreign key & misc cleanup
    
    * Remove codeset-related code
    
    * Error handling when duplicate contributor exists
    
    ---------
    
    Co-authored-by: Dylan Rogowsky <dylan@oxd.com>

--- api/src/__mocks__/db.ts
+++ api/src/__mocks__/db.ts
@@ -103,6 +103,18 @@ export class MockRes {
   });
 
   /**
+   * The value of the last `.sendStatus(<value>)` call.
+   *
+   * @type {*}
+   * @memberof MockRes
+   */
+  sendStatusValue: any;
+  sendStatus = sinon.fake((value: any) => {
+    this.sendStatusValue = value;
+    return this;
+  });
+
+  /**
    * The value of the last `.send(<value>)` call.
    *
    * @type {*}
