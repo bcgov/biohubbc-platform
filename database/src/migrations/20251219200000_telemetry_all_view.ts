d3a96ccf781e77f1d040bced32f900671bc0f592
Annika Meijer <annika.meijer@gov.bc.ca>
2026-01-15 16:13:11 -0800

    updating views to use ctes

--- database/src/migrations/20251219200000_telemetry_all_view.ts
+++ database/src/migrations/20251219200000_telemetry_all_view.ts
@@ -9,26 +9,38 @@ import { Knex } from 'knex';
  */
 export async function up(knex: Knex): Promise<void> {
   await knex.raw(`
-    CREATE MATERIALIZED VIEW bcgw.telemetry_all AS
+CREATE MATERIALIZED VIEW bcgw.telemetry_all AS
+WITH deployments AS (
     SELECT
-      sf.submission_feature_id,
-      sf.uuid,
-      sf.submission_id,
-      sf.source_id,
-      sf.data->>'device_id' as device_id,
-      (sf.data->>'latitude')::numeric as latitude,
-      (sf.data->>'longitude')::numeric as longitude,
-      (sf.data->>'timestamp')::timestamptz as timestamp,
-      (sf.data->>'dop')::numeric as dop,
-      dep.data->>'device_key' as device_key,
-      dep.data->>'animal_id' as animal_id
-    FROM biohub.submission_feature sf
-    JOIN biohub.feature_type ft ON sf.feature_type_id = ft.feature_type_id
-    LEFT JOIN biohub.submission_feature dep ON dep.submission_feature_id = sf.parent_submission_feature_id
-      AND dep.feature_type_id = (SELECT feature_type_id FROM biohub.feature_type WHERE name = 'telemetry_deployment')
-    WHERE ft.name = 'telemetry'
-      AND sf.record_end_date IS NULL
-      AND (sf.data->>'timestamp')::timestamptz <= (NOW() - INTERVAL '4 months');
+        dep.submission_feature_id,
+        dep.data->>'device_key' AS device_key,
+        dep.data->>'animal_id' AS animal_id
+    FROM biohub.submission_feature dep
+    JOIN biohub.feature_type ft_dep
+      ON dep.feature_type_id = ft_dep.feature_type_id
+    WHERE ft_dep.name = 'telemetry_deployment'
+      AND dep.record_end_date IS NULL
+)
+SELECT
+    sf.submission_feature_id,
+    sf.uuid,
+    sf.submission_id,
+    sf.source_id,
+    sf.data->>'device_id' AS device_id,
+    (sf.data->>'latitude')::numeric AS latitude,
+    (sf.data->>'longitude')::numeric AS longitude,
+    (sf.data->>'timestamp')::timestamptz AS timestamp,
+    (sf.data->>'dop')::numeric AS dop,
+    d.device_key,
+    d.animal_id
+FROM biohub.submission_feature sf
+JOIN biohub.feature_type ft
+  ON sf.feature_type_id = ft.feature_type_id
+LEFT JOIN deployments d
+  ON d.submission_feature_id = sf.parent_submission_feature_id
+WHERE ft.name = 'telemetry'
+  AND sf.record_end_date IS NULL
+  AND (sf.data->>'timestamp')::timestamptz <= (NOW() - INTERVAL '4 months');
   `);
 }
 
