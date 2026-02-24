5135a7cd9b9a5013ae0ce2fb3581cc6492b2aee4
Josiah <josiah.pilon@gmail.com>
2026-02-24 13:54:59 -0800

    test: TeamAuthorizationRepository

---
+++ api/src/repositories/authorization/team-authorization-repository.test.ts
@@ -0,0 +1,81 @@
+import { expect } from 'chai';
+import { describe } from 'mocha';
+import { QueryResult } from 'pg';
+import sinon from 'sinon';
+import { getMockDBConnection } from '../../__mocks__/db';
+import { TeamAuthorizationRepository } from './team-authorization-repository';
+
+describe('TeamAuthorizationRepository', () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  describe('findTeamMembership', () => {
+    it('returns a record when the user is an active member of the team', async () => {
+      const mockRow = { team_member_id: 'tm-1' };
+      const mockResponse = { rowCount: 1, rows: [mockRow] } as unknown as Promise<QueryResult<any>>;
+      const mockConnection = getMockDBConnection({ knex: async () => mockResponse });
+
+      const repository = new TeamAuthorizationRepository(mockConnection);
+      const result = await repository.findTeamMembership(1, 'team-1');
+
+      expect(result).to.eql(mockRow);
+    });
+
+    it('returns null when the user is not a member of the team', async () => {
+      const mockResponse = { rowCount: 0, rows: [] } as unknown as Promise<QueryResult<any>>;
+      const mockConnection = getMockDBConnection({ knex: async () => mockResponse });
+
+      const repository = new TeamAuthorizationRepository(mockConnection);
+      const result = await repository.findTeamMembership(1, 'team-1');
+
+      expect(result).to.be.null;
+    });
+  });
+
+  describe('findTeamMembershipByPolicy', () => {
+    it('returns a record when the user has team access to the policy', async () => {
+      const mockRow = { team_policy_id: 'tp-1' };
+      const mockResponse = { rowCount: 1, rows: [mockRow] } as unknown as Promise<QueryResult<any>>;
+      const mockConnection = getMockDBConnection({ knex: async () => mockResponse });
+
+      const repository = new TeamAuthorizationRepository(mockConnection);
+      const result = await repository.findTeamMembershipByPolicy(1, 'policy-1');
+
+      expect(result).to.eql(mockRow);
+    });
+
+    it('returns null when the user does not have team access to the policy', async () => {
+      const mockResponse = { rowCount: 0, rows: [] } as unknown as Promise<QueryResult<any>>;
+      const mockConnection = getMockDBConnection({ knex: async () => mockResponse });
+
+      const repository = new TeamAuthorizationRepository(mockConnection);
+      const result = await repository.findTeamMembershipByPolicy(1, 'policy-1');
+
+      expect(result).to.be.null;
+    });
+  });
+
+  describe('findTeamMembershipByDataRequest', () => {
+    it('returns a record when the user has team access to the data request', async () => {
+      const mockRow = { data_request_id: 'dr-1' };
+      const mockResponse = { rowCount: 1, rows: [mockRow] } as unknown as Promise<QueryResult<any>>;
+      const mockConnection = getMockDBConnection({ knex: async () => mockResponse });
+
+      const repository = new TeamAuthorizationRepository(mockConnection);
+      const result = await repository.findTeamMembershipByDataRequest(1, 'dr-1');
+
+      expect(result).to.eql(mockRow);
+    });
+
+    it('returns null when the user does not have team access to the data request', async () => {
+      const mockResponse = { rowCount: 0, rows: [] } as unknown as Promise<QueryResult<any>>;
+      const mockConnection = getMockDBConnection({ knex: async () => mockResponse });
+
+      const repository = new TeamAuthorizationRepository(mockConnection);
+      const result = await repository.findTeamMembershipByDataRequest(1, 'dr-1');
+
+      expect(result).to.be.null;
+    });
+  });
+});
