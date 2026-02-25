f35d24e9dad82843912fd7bdbe13e3f60f8cbf9f
Josiah <josiah.pilon@gmail.com>
2026-02-25 12:09:54 -0800

    fix: misc

--- api/src/repositories/data-request-status-repository.ts
+++ api/src/repositories/data-request-status-repository.ts
@@ -90,7 +90,7 @@ export class DataRequestStatusRepository extends BaseRepository {
 
     if (response.rowCount !== 1) {
       throw new ApiExecuteSQLError('Failed to create data request status', [
-        'DataRequestRepository->createDataRequestStatus',
+        'DataRequestStatusRepository->createDataRequestStatus',
         'rowCount !== 1'
       ]);
     }
