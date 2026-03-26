3a4b043bdfc16d9e2609c7c9846828699320aa8c
Macgregor Aubertin-Young <macgregor.aubertin-young@gov.bc.ca>
2026-03-25 23:33:47 -0700

    contributor seeds

--- api/src/request-handlers/security/authorization.test.ts
+++ api/src/request-handlers/security/authorization.test.ts
@@ -6,6 +6,7 @@ import sinonChai from 'sinon-chai';
 import { HTTPError } from '../../errors/http-error';
 import { SystemUserExtended } from '../../repositories/user-repository';
 import { AuthorizationService } from '../../services/authorization/authorization-service';
+import { ContributorSystemUserService } from '../../services/contributor-system-user-service';
 import { getRequestHandlerMocks, registerMockDBConnection } from '../../__mocks__/db';
 import * as authorization from './authorization';
 
@@ -208,6 +209,34 @@ describe('authorizeRequest', function () {
 
     expect(isAuthorized).to.equal(true);
     expect(executeAuthorizationSchemeStub).to.have.been.calledOnce;
+    expect(mockReq.contributor_id).to.equal(77);
+  });
+
+  it('uses req.system_user.system_user_id for Contributor authorization (not DB connection system user id)', async function () {
+    registerMockDBConnection({
+      systemUserId: () => 999
+    });
+
+    sinon.stub(AuthorizationService.prototype, 'authorizeSystemAdministrator').resolves(false);
+
+    const findContributorSystemUserStub = sinon
+      .stub(ContributorSystemUserService.prototype, 'findContributorSystemUser')
+      .resolves({
+        contributor_system_user_id: 1,
+        contributor_id: 77,
+        system_user_id: 12
+      });
+
+    const mockReq = {
+      authorization_scheme: { and: [{ discriminator: 'Contributor' }] },
+      keycloak_token: { sub: 'some-guid' },
+      system_user: { system_user_id: 12 }
+    } as unknown as Request;
+
+    const isAuthorized = await authorization.authorizeRequest(mockReq);
+
+    expect(isAuthorized).to.equal(true);
+    expect(findContributorSystemUserStub).to.have.been.calledOnceWith(12);
     expect(mockReq.contributor_id).to.equal(77);
   });
 });
