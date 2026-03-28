ceeacfb301fcfa6c5ea7ced8e5c228b79695934b
Josiah <josiah.pilon@gmail.com>
2026-03-27 18:07:06 -0700

    feat: adds getTicketForUser route handler

---
+++ api/src/paths/tickets/{ticketId}/index.ts
@@ -0,0 +1,73 @@
+import { RequestHandler } from 'express';
+import { Operation } from 'express-openapi';
+import { getDBConnection } from '../../../database/db';
+import { TicketWithHistorySchema } from '../../../openapi/schemas/admin-ticket';
+import { defaultErrorResponses } from '../../../openapi/schemas/http-responses';
+import { authorizeRequestHandler } from '../../../request-handlers/security/authorization';
+import { TicketService } from '../../../services/ticket-service';
+import { getLogger } from '../../../utils/logger';
+
+const defaultLog = getLogger('paths/administrative/tickets/{ticketId}');
+
+export const GET: Operation = [
+  authorizeRequestHandler(() => ({
+    and: [{ discriminator: 'SystemUser' }]
+  })),
+  getTicketForUser()
+];
+
+GET.apiDoc = {
+  description: 'Get ticket details by ticket ID',
+  tags: ['tickets'],
+  security: [
+    {
+      Bearer: []
+    }
+  ],
+  parameters: [
+    {
+      in: 'path',
+      name: 'ticketId',
+      required: true,
+      schema: {
+        type: 'string',
+        format: 'uuid'
+      },
+      description: 'Ticket UUID.'
+    }
+  ],
+  responses: {
+    200: {
+      description: 'Ticket retrieved successfully',
+      content: {
+        'application/json': {
+          schema: TicketWithHistorySchema
+        }
+      }
+    },
+    ...defaultErrorResponses
+  }
+};
+
+export function getTicketForUser(): RequestHandler {
+  return async (req, res) => {
+    const connection = getDBConnection(req.keycloak_token);
+
+    try {
+      await connection.open();
+
+      const ticketService = new TicketService(connection);
+      const ticket = await ticketService.getTicket(req.params.ticketId);
+
+      await connection.commit();
+
+      return res.status(200).json(ticket);
+    } catch (error) {
+      defaultLog.error({ label: 'getTicket', message: 'error', error });
+      await connection.rollback();
+      throw error;
+    } finally {
+      connection.release();
+    }
+  };
+}
